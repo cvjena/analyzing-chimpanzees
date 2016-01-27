0a642990bc1f8ab263d4644769539cb498b1bf1d
Alexander Freytag <alexander.freytag@uni-jena.de>
2016-01-27 18:16:19 +0100

    added ground truth reader for age grouups

---
+++ age_group_classification/age_group_classifier_ground_truth.m
@@ -0,0 +1,18 @@
+function str_out = age_group_classifier_ground_truth ( str_extracted_features, str_settings )
+% 
+%  BRIEF:
+% 
+%  INPUT:
+% 
+%  OUTPUT:
+%     str_out.s_names = estimated names of the detected chimpansees in the image
+
+    s_fn          = str_settings.s_fn;
+    s_age_groups  = getStringDataSingleImage ( s_fn, 'AgeGroup' );
+
+
+    %% assign output variables
+    str_out              = [];
+    str_out.s_age_groups = s_age_groups;        
+    
+end
