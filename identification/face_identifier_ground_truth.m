25fba837b04f1beda87629f16dd95613ee016a3f
Alexander Freytag <alexander.freytag@uni-jena.de>
2016-01-27 18:12:21 +0100

    face identification implemented

--- identification/face_identifier_ground_truth.m
+++ identification/face_identifier_ground_truth.m
@@ -1,16 +1,18 @@
-function str_out = face_identifier_ground_truth ( image, str_boxes, str_settings )
+function str_out = face_identifier_ground_truth ( str_extracted_features, str_settings )
 % 
 %  BRIEF:
 % 
 %  INPUT:
 % 
 %  OUTPUT:
-%     str_out.s_ids = estimated names of the detected chimpansees in the image
+%     str_out.s_names = estimated names of the detected chimpansees in the image
 
-     s_namesTmp  = getStringDataSingleImage ( s_fn, 'Identity' );
+    s_fn     = str_settings.s_fn;
+    s_names  = getStringDataSingleImage ( s_fn, 'Identity' );
 
 
     %% assign output variables
-    str_out = str_regions;        
+    str_out         = [];
+    str_out.s_names = s_names;        
     
 end
--- identification/face_identifier_linear_SVM.m
+++ identification/face_identifier_linear_SVM.m
@@ -1,13 +1,26 @@
-function str_out = face_identifier_linear_SVM ( image, str_boxes, str_settings )
+function str_out = face_identifier_linear_SVM ( str_extracted_features, str_settings )
 % 
 %  BRIEF:
 % 
 %  INPUT:
 % 
 %  OUTPUT:
-%     str_out.s_ids = estimated names of the detected chimpansees in the image
+%     str_out.s_names = estimated names of the detected chimpansees in the image
+
+    svmmodel          = getFieldWithDefault ( str_settings, 'svmmodel', [] );
+    settingsLibLinear = getFieldWithDefault ( str_settings, 'settingsLibLinear', [] );    
+    
+    assert ( ~isempty ( svmmodel ),          'No trained SVM model for identification provided!' );
+    assert ( ~isempty ( settingsLibLinear ), 'No LibSVM settings for identification provided!' );
+        
+
+   [predicted_labels, ~, ~] = liblinear_test ( zeros(1,size(str_extracted_features,2)), sparse(double(str_extracted_features')), svmmodel, settingsLibLinear );
+   
+    %FIXME change towards returning actual names instead of numbers!
+    predicted_names = arrayfun ( @num2string, predicted_labels );
 
     %% assign outputs
-    str_out       = [];
-    str_out.s_ids = [];
+    str_out         = [];
+    str_out.s_names = predicted_names;
+    
 end
